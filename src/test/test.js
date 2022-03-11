45e3b3d3d81e0e6a21a3d91493b4590b879f94a1
Julia L <92579633+jull92@users.noreply.github.com>
2022-03-11 15:17:24 +0000

    Delete src/test directory

--- src/test/test.js
+++
@@ -1,5 +0,0 @@
-test("shoudl check if server is listening on port", async () => {
-    const response = await request.get("/health");
-    expect(response.status).toBe(200);
-    expect(response.body.msg).toBe("Server is up")
-});
