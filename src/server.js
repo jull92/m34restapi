c7aff2dede74701762e74444ddf642dcd998fb1f
jull92 <julia.kata92@gmail.com>
2022-03-17 16:25:41 +0000

    this should work now

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ require("./db/connection");
 const express = require("express");
 const movieRouter = require("./movie/movieRoutes");
 const userRouter = require("./user/userRoutes");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
+// methods above routers so that the routers have access to them
+
 app.use(express.json());
+app.use(cors());
 app.use(movieRouter);
 app.use(userRouter);
 
